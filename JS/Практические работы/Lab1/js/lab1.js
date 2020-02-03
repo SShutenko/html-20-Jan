c6240020683b95894645283a47ae59887b7768b7
SShutenko <1_fantasy@mail.ru>
2020-02-03 23:54:00 +0600

    раскидка по папкам. JS начало

---
+++ JS/Практические работы/Lab1/js/lab1.js
@@ -0,0 +1,8 @@
+function next() {
+	let e=document.getElementById("out");
+	e.innerHTML="+---+"+"<br>"+"|   |"+"<br>"+"|   |"+"<br>"+"+---+";
+	}
+	function cleard() {
+	let e=document.getElementById("out");
+	e.innerHTML="&nbsp;";
+}
